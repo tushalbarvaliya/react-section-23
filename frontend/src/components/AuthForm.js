9dfb1989c98521f0fa77975a6b60bbe96188bd2d
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-11 15:30:41 +0530

    fix : button change on submitting .

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,7 +1,13 @@
 // import { useState } from 'react';
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
   // const [isLogin, setIsLogin] = useState(true);
@@ -9,13 +15,26 @@ function AuthForm() {
   // function switchAuthHandler() {
   //   setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
   // }
-
+  const data = useActionData();
   const [searchParams] = useSearchParams();
-  const isLogin = searchParams.get('mode')==='login';
+  const isLogin = searchParams.get("mode") === "login";
+
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -25,10 +44,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? 'signup':'login'}`}>
-            {isLogin ? 'Create new user' : 'Login'}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
