613e0c5e7d366b959978c3bc10085a6ed54b5860
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-11 17:26:28 +0530

    feat : add route protection.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,10 +14,11 @@ import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
-import { action as logoutAction, tokenLoader } from "./pages/Logout";
+import { action as logoutAction } from "./pages/Logout";
 import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
+import { checkAuthLoader, tokenLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
@@ -51,6 +52,7 @@ const router = createBrowserRouter([
                 path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -58,6 +60,7 @@ const router = createBrowserRouter([
             path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,11 +1,5 @@
 import { redirect } from "react-router-dom";
-import { getAuthToken } from "../util/auth";
-
 export function action() {
   localStorage.removeItem("token");
   return redirect("/");
-}
-
-export function tokenLoader() {
-  return getAuthToken();
 }
