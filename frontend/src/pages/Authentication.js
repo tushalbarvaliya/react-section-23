8c550c5cba339ee490bfaf8597498ec569e661ca
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-11 15:11:32 +0530

    feat : authentication form submit to backend.

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,47 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response(JSON.stringify({ message: "Unsupported Mode" }), {
+      status: 422,
+    });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    return new Response(
+      JSON.stringify({ message: "Could Not Authenticate User." }),
+      { status: 500 },
+    );
+  }
+
+  // manage that token here
+  return redirect("/");
+}
